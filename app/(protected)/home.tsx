bbca398ca00f58cc8a4537d6395c89fad4a7dfa4
carolly <carolly.barbosa@gmail.com>
2026-03-18 23:35:28 -0300

    feat: adiciona animação na home

--- app/(protected)/home.tsx
+++ app/(protected)/home.tsx
@@ -16,9 +16,35 @@ import {
   Text,
   View,
 } from 'react-native';
+import Animated, {
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import { CartesianChart, Line } from 'victory-native';
 
 export default function Home() {
+  const fadeAnim = useSharedValue(0.2);
+  const scrollY = useSharedValue(0);
+
+  useAnimatedReaction(
+    () => scrollY.value,
+    (value) => {
+      if (value > 350) {
+        fadeAnim.value = withTiming(1, { duration: 700 });
+      } else {
+        fadeAnim.value = withTiming(0.2, { duration: 400 });
+      }
+    },
+  );
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: fadeAnim.value,
+    };
+  });
+
   const { logout, user } = useAuth();
   const {
     transactions,
@@ -48,6 +74,10 @@ export default function Home() {
         style={styles.scrollContainer}
         contentContainerStyle={styles.container}
         showsVerticalScrollIndicator={false}
+        onScroll={(event) => {
+          scrollY.value = event.nativeEvent.contentOffset.y;
+        }}
+        scrollEventThrottle={16}
       >
         {/* Header */}
         <View style={styles.header}>
@@ -88,32 +118,34 @@ export default function Home() {
           </View>
         </View>
 
-        <View style={{ ...styles.header, marginTop: 24 }}>
-          <Text style={styles.greeting}>{`Últimas transações`}</Text>
-        </View>
+        <Animated.View style={[reanimatedStyle]}>
+          <View style={{ ...styles.header, marginTop: 24 }}>
+            <Text style={styles.greeting}>{`Últimas transações`}</Text>
+          </View>
 
-        <View>
-          {loading ? (
-            <ActivityIndicator
-              color='#2da12b'
-              size='large'
-              style={{ marginTop: 40 }}
-            />
-          ) : transactions.slice(0, 10).length > 0 ? (
-            transactions
-              .slice(0, 10)
-              .map((item) => (
-                <TransactionItem
-                  key={item.id}
-                  item={item}
-                  deleting={deletingId === item.id}
-                  onDelete={onDelete}
-                />
-              ))
-          ) : (
-            <Text>Nenhuma transação ainda.</Text>
-          )}
-        </View>
+          <View>
+            {loading ? (
+              <ActivityIndicator
+                color='#2da12b'
+                size='large'
+                style={{ marginTop: 40 }}
+              />
+            ) : transactions.slice(0, 10).length > 0 ? (
+              transactions
+                .slice(0, 10)
+                .map((item) => (
+                  <TransactionItem
+                    key={item.id}
+                    item={item}
+                    deleting={deletingId === item.id}
+                    onDelete={onDelete}
+                  />
+                ))
+            ) : (
+              <Text>Nenhuma transação ainda.</Text>
+            )}
+          </View>
+        </Animated.View>
 
         {/* Logout Button */}
         <Pressable
